cbd7952668f8ea794ddcfc6984aabba311968e74
Allbor666 <allbor347@gmail.com>
2024-10-10 23:46:48 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-def generate_password(number):
-    password = ""
-    pairs = []
-    # Генерируем пары чисел от 1 до 10
-    for i in range(1, 11):
-        for j in range(i + 1, 11):  # чтобы избежать дубликатов (1,2) и (2,1)
-            pair_sum = i + j
-            # Проверяем кратность
-            if pair_sum != 0 and number % pair_sum == 0:
-                pairs.append((i, j))
-    # Формируем пароль
-    for pair in pairs:
-        password += str(pair[0]) + str(pair[1])
-    return password
-number = 9  # Например, число из первой вставки
-password = generate_password(number)
-print(password)
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+def is_contains(string, list_to_search):
+    count_calls()
+    string = string.lower()
+    for item in list_to_search:
+        if string == item.lower():
+            return True
+    return False
+print(string_info('Capybara')) # Пример вызова string_info
+print(string_info('Armageddon')) # Пример вызова string_info
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Пример вызова is_contains
+print(is_contains('cycle', ['recycling', 'cyclic']))
+
+print(calls)
