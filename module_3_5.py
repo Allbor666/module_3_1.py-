672de19583092f4565c5212e6b099bb1080d3332
Allbor666 <allbor347@gmail.com>
2024-10-12 18:50:24 +0700

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,10 +1,12 @@
 def get_multiplied_digits(number):
-    str_number = str(number)  # Преобразуем число в строку
-    if len(str_number) > 1:# Если длина строки больше 1, продолжаем работу
-        first = int(str_number[0])  # Берем первую цифру
-        return first * get_multiplied_digits(int(str_number[1:]))# Рекурсивно вызываем функцию с оставшимися цифрами
+    str_number = str(number).lstrip('0')  # Удаляем нули из начала строки
+    if str_number == '':  # Если строка пустая, возвращаем 1
+        return 1
+    first = int(str_number[0])  # Извлекаем первую цифру и преобразуем в int
+    if len(str_number) > 1:         # Если длина строки больше 1, продолжаем рекурсию
+        return first * get_multiplied_digits(
+            int(str_number[1:]))  # Умножаем первую цифру на результат рекурсивного вызова
     else:
-        return int(str_number)# Если остается одна цифра, просто возвращаем её
-
-result = get_multiplied_digits(40203)
+        return first  # Если длина 1, возвращаем первую цифру
+result = get_multiplied_digits(420)# Пример использования функции
 print(result)
